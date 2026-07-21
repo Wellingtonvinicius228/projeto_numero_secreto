4e4bdf82e4401d4fd693e26374add37831e7efec
wellington <vinizingp14@gmail.com>
2026-07-21 14:52:02 -0300

    limite de 10 para 100

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroLimite = 100;
+let numeroSecreto = gerarNumeroAleatorio();N
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
